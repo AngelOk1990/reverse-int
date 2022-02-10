1c99963fbbb91dc11370dafd399afe037f4aec9f
AngelOk1990 <pandafree@mail.ru>
2022-02-10 16:26:49 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
-}
+  let str = "" + Math.abs(n);
+  let result = "";
+for (let i = 0; i < str.length; i++) {
+    //if (result[1] === 0) {
+      //  result = `${str[i]}${result.substring(1)}`;
+    //} else if ((result[1] === 0) & (result[2] === 0)) {
+   //     result = `${str[i]}${result.substring(2)}`
+   // } else {
+        result = `${str[i]}${result}`
+    }
+    return result;
+  }
+//}
